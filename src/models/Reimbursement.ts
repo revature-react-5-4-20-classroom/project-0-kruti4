0efd08368d8150bf040a85e8e74156ca8a938472
kruti4 <krutifaldu9@gmail.com>
2020-05-14 18:57:02 -0700

    add sql file

--- src/models/Reimbursement.ts
+++ src/models/Reimbursement.ts
@@ -4,17 +4,19 @@ export default class Reimbursement {
     amount: number;  // not null
     dateSubmitted: number; // not null
     dateResolved: number; // not null
-    description: string; // not null        resolver: number, // foreign key -> User
+    description: string; // not null       
+    resolver: number; // foreign key -> User
     status: number; // foreign ey -> ReimbursementStatus, not null
     type: number;// foreign key -> ReimbursementType
 
-    constructor(reimbursementId: number, author: number, amount: number, dateSubmitted: number, dateResolved: number, description: string, status: number, type: number) {
+    constructor(reimbursementId: number, author: number, amount: number, dateSubmitted: number, dateResolved: number, description: string,resolver:number, status: number, type: number) {
         this.reimbursementId = reimbursementId;
         this.author = author;
         this.amount = amount;
         this.dateResolved = dateResolved;
         this.dateSubmitted = dateSubmitted;
         this.description = description;
+        this.resolver=resolver;
         this.status = status;
         this.type = type;
     }
